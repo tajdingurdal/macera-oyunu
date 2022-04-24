f5438e026de9d26fe471f70150597cc161fd2020
tajdingurdal <tjdngrdl1@gmail.com>
2022-04-25 01:47:27 +0300

    iyilestirme yapıldı

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Game.start();
 
-//		Snake s = new Snake(null, 0);
-//		System.out.println(s.damage);
-//		System.out.println(s.ad);
-
-	}
-
-}
-
-class Snake {
-	String ad = "Yýlan";
-	int damage = 0;
-
-	public Snake(String ad, int damage) {
-		super();
-		ad = this.ad;
-
-		Random r = new Random();
-		int olasýlýk = (r.nextInt(4) + 1);
-		if (olasýlýk == 1) {
-			this.damage = 3;
-		}
-		if (olasýlýk == 2) {
-			this.damage = 4;
-		}
-		if (olasýlýk == 3) {
-			this.damage = 5;
-		}
-		if (olasýlýk == 4) {
-			this.damage = 6;
-		}
 	}
 
 }
